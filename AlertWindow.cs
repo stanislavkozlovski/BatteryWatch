3ce6cf6a14c46c0fcfe03fa4553e56ecde5ea305
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2016-09-18 13:07:26 +0300

    Created an AlertWindow.cs form that will show up whenever the battery exceeds the min/max treshold.

---
+++ AlertWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatteryWatch
+{
+    public partial class AlertWindow : Form
+    {
+        public AlertWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
